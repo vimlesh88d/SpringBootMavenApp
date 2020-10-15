dd65acffc31aaf29e527f25c1ba58084f5ec5fbe
vimlesh88d <kumarvimlesh.it@gmail.com>
2020-10-15 19:50:59 +0530

    added jsp by Ak

--- MavenApp/src/main/java/com/devops/MavenApp/TestController.java
+++ MavenApp/src/main/java/com/devops/MavenApp/TestController.java
@@ -2,12 +2,15 @@ package com.devops.MavenApp;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class TestController {
 	@RequestMapping("/hello")
-	public String hello() {
-		return "Welcome to India";
+	public ModelAndView hello() {
+		String message = "Welcome to new India22";
+		return new ModelAndView("welcome", "message", message);
+
 	}
 
 }
---
+++ MavenApp/src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,5 @@
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<h1>${message}<h1/>
+<html/>
